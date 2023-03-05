ce6943501701f683ed698783fd7e7131aa2ec9e7
lFARIZl <fariz@code.edu.az>
2023-03-05 09:49:02 -0800

    Task2Done

---
+++ Task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float TotalAzerbaijanBudget = 20000000;
+            float TotalPayMinistryOfDefence = TotalAzerbaijanBudget * 0.2F;
+            float TotalPayMinistryOfHighTechnologies = TotalAzerbaijanBudget * 0.1F;
+            float RestOfBudget = TotalAzerbaijanBudget - TotalPayMinistryOfDefence - TotalPayMinistryOfHighTechnologies;
+
+            Console.WriteLine("Büdcə: " + TotalAzerbaijanBudget);
+            Console.WriteLine("Müdafiə Nazirliyinə: " + TotalPayMinistryOfDefence);
+            Console.WriteLine("Yüksək Texnologiyalar Nazirliyinə: " + TotalPayMinistryOfHighTechnologies);
+            Console.WriteLine("Yerdə qalan büdcə: " + RestOfBudget);
+        }
+    }
+}
